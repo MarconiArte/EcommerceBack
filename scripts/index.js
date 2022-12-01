84034bf2d52a2e1977a2dbedf5238cb934c5f4ce
Marcos Niz <marconi4305@gmail.com>
2022-12-01 11:07:37 -0300

    Se agrego el maquetado de carrito y se corregio css

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,10 @@ const btnMenu = document.querySelector('#btnMenu')
 btnMenu.addEventListener('click', () =>{
     nav.classList.toggle('hidden');
 })
+
+const carritoContenedor = document.querySelector('#carrito-contenedor')
+const carritoIco = document.querySelector('#carritoIco')
+
+carritoIco.addEventListener('click', () =>{
+    carritoContenedor.classList.toggle('hidden')
+})
